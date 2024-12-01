a9ff5c6a18d761df40248d6d28a3fe747d341555
prosk67 <proyashsk@gmail.com>
2024-12-01 18:15:39 +0600

    Updated Add Product Section

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
@@ -11,7 +12,7 @@ class AdminController extends Controller
     {
         $data = Category::all();
 
-        return view('admin.category',compact('data'));
+        return view('admin.category', compact('data'));
     }
     public function add_category(Request $request)
     {
@@ -34,13 +35,13 @@ class AdminController extends Controller
     public function edit_category($id)
     {
         $data = Category::find($id);
-        return view('admin.edit_category',compact('data'));
+        return view('admin.edit_category', compact('data'));
     }
 
-    public function update_category(Request $request,$id)
+    public function update_category(Request $request, $id)
     {
         $data = Category::find($id);
-        $data->category_name = $request->category ;
+        $data->category_name = $request->category;
         $data->save();
         toastr()->timeOut(1000)->addUpdated('Category');
         return redirect('/view_category');
@@ -48,7 +49,30 @@ class AdminController extends Controller
     public function add_product()
     {
         $category = Category::all();
-        return view('admin.add_product',compact('category'));
+        return view('admin.add_product', compact('category'));
+
+    }
+    public function upload_product(Request $request)
+    {
+        $data = new Product;
+        $data->title = $request->title;
+        $data->description = $request->description;
+        $data->price = $request->price;
+        $data->quantity = $request->qty;
+        $data->category = $request->category;
+
+        $image = $request->image;
+        if($image)
+        {
+            $imagename = time().'.'.$image->getClientOriginalExtension();
+
+            $request->image->move('products',$imagename);
+            $data->image = $imagename;
+        }
+
+        $data->save();
+        toastr()->timeOut(1000)->addUpdated('Product');
+        return redirect()->back();
 
     }
 }
