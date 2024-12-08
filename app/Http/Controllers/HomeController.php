d8435084561a44c7415f5cd021641f681480ac7e
prosk67 <proyashsk@gmail.com>
2024-12-08 16:22:24 +0600

    Added Cart Total Items

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,20 +17,53 @@ class HomeController extends Controller
 
     public function home(){
         $product = Product::all();
-        return view('home.index',compact('product'));
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+        }
+        else
+        {
+            $count = '';
+        }
+        
+
+        return view('home.index',compact('product','count'));
     }
 
     public function login_home()
     {
         $product = Product::all();
-        return view('home.index',compact('product'));
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+        }
+        else
+        {
+            $count = '';
+        }
+        
+        return view('home.index',compact('product','count'));
 
     }
     public function product_details($id)
     {
         $data = Product::find($id);
+        if(Auth::id())
+        {
+            $user = Auth::user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+        }
+        else
+        {
+            $count = '';
+        }
         
-        return view('home.product_details',compact('data'));
+        return view('home.product_details',compact('data','count'));
     }
 
     public function add_cart($id)
--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -38,8 +38,9 @@
           @if (Route::has('login'))
 
             @auth
-            <a href="">
+            <a href="{{url('mycart')}}">
               <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+              [{{$count}}]
             </a>
             <form style="padding: 15px" method="POST" action="{{ route('logout') }}">
               @csrf
